6e14a924f42cfe2161b6ce9e41bc313ce2618ec3
ritam1997 <61182781+ritam1997@users.noreply.github.com>
2021-04-29 09:18:47 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,24 @@
+#Bug456 comment before add function
+def add(x, y):
+	return x + y
+def sub(x, y):
+	if(x>y):
+		return x - y
+	else:
+		return y - x
+def mul(x, y):
+	return x * y
+def div(x, y):
+	if(y!=0):
+		return x / y # Bug789 EDIT
+	else:
+		return 'Nan'
+def fact(x):
+	if(x==0):
+		return 1
+	else
+		return x * fact(x-1)
+def rev(x): #reverse method
+	return str(x)[::-1]
+def fact(x):
+	pass
